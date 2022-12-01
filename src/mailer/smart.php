926991af3200db8b409a9d420ffe88e597d571f1
Anna <anna.a.khaustova@gmail.com>
2022-12-01 14:31:32 +0300

    I've changed address of email

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -11,15 +11,15 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'anna.a.khaustova@gmail.com';                 // Наш логин
-$mail->Password = 'Obihoh0093';                           // Наш пароль от ящика
+$mail->Username = 'Loramoonlight@yandex.ru';                 // Наш логин
+$mail->Password = 'qctmhxibbxprywqn';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('anna.a.khaustova@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('rico48@mail.ru');     // Add a recipient
+$mail->setFrom('Loramoonlight@yandex.ru', 'Pulse');   // От кого письмо 
+$mail->addAddress('anna.a.khaustova@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
